91a88093f7841459932b930d3e7c92a99befe845
submergew <wangziyu0412@outlook.com>
2024-07-31 23:33:15 +0800

    const修饰常量

--- chapter1/const.c
+++ chapter1/const.c
@@ -6,27 +6,24 @@
 #include <string.h>
 
 
-const int a = 10; //  全局const常亮  放在常量区的保护
+// int a = 10;
+const int a = 10;//  全局const常亮  放在常量区的保护
 
-void testf()
-{int *p = &a;
-    *p = 100;
-printf("%d\n",a);
-
+void testf() {
+    int *p = &a;
+    // *p = 100; // 常量不能修改值
+    printf("%d\n", a);
 }
 
-//局部const修饰常量
-void testg()
-{
-    const int b = 10; //数据存放在栈上
+// 局部const修饰常量
+void testg() {
+    const int b = 10;//数据存放在栈上
     int *p = &b;
     *p = 100;
-    printf("%d\n",b);
-
+    printf("%d\n", b);
 }
-int main()
-{
-   testf();
-   testg();
+int main() {
+    testf();
+    testg();
     return 0;
 }
