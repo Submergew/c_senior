a17c9c479aaf2a65c417c292f8fd4c85d8e0b927
submergew <wangziyu0412@outlook.com>
2024-07-31 23:18:05 +0800

    const修饰常量

---
+++ chapter1/const.c
@@ -0,0 +1,32 @@
+//
+// Created by wangz on 2024/7/31.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+const int a = 10; //  全局const常亮  放在常量区的保护
+
+void testf()
+{int *p = &a;
+    *p = 100;
+printf("%d\n",a);
+
+}
+
+//局部const修饰常量
+void testg()
+{
+    const int b = 10; //数据存放在栈上
+    int *p = &b;
+    *p = 100;
+    printf("%d\n",b);
+
+}
+int main()
+{
+   testf();
+   testg();
+    return 0;
+}
